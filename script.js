a768c54d6acc7e5688ad69b8341e43481f12d13e
AF Tamim <itamim345@gmail.com>
2022-10-01 03:56:48 +0530

    JS Done

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //Selectors
 inputTextFiled = document.getElementById('message');
 submitBtn = document.getElementById('clip-btn');
-//Function to copy on clipboard
+
+//Events Handler to copy on clipboard
 submitBtn.addEventListener('click',function(){
     inputTextFiled.select()
     document.execCommand('Copy')
